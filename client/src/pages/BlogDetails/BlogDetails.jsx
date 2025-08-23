9262087f9c5f25e155ecb7bea0ea886724a24cc1
OM SONANI <sonaniom2004@gmail.com>
2025-08-23 16:25:13 +0530

    Done

--- client/src/pages/BlogDetails/BlogDetails.jsx
+++ client/src/pages/BlogDetails/BlogDetails.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import "./blogcontent.css";
+import "./blogContent.css";
 
 export default function BlogPage() {
     const { id } = useParams();
